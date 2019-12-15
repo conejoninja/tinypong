55af77e4bf2e745851b4580507608a1ecf2d16c5
Daniel Esteban <conejo@theninjabunny.com>
2019-12-15 20:36:04 +0100

    working

--- main.go
+++ main.go
@@ -13,14 +13,40 @@ import (
 	"tinygo.org/x/drivers/hub75"
 )
 
-var display hub75.Device
-var colors []color.RGBA
-var rtc ds3231.Device
-var dt time.Time
-var err error
-var timeStr []byte
+const (
+	IDLE   Status = 0
+	PLAY   Status = 1
+	WINNER Status = 2
+)
+
+type Status uint8
+
+type Game struct {
+	display hub75.Device
+	colors  []color.RGBA
+	rtc     ds3231.Device
+	dt      time.Time
+	timeStr []byte
+	players [2]Player
+	ball    Ball
+	status  Status
+}
+
+type Player struct {
+	pin     machine.ADC
+	y       int16
+	lastY   int16
+	targetY float32
+	score   uint8
+}
+
+type Ball struct {
+	x, y, vx, vy float32
+}
 
 func main() {
+	var game Game
+	var err error
 	// SPI for the hub75
 	machine.SPI0.Configure(machine.SPIConfig{
 		Frequency: 8000000,
@@ -28,41 +54,43 @@ func main() {
 	// I2C for the RTC DS3231
 	machine.I2C0.Configure(machine.I2CConfig{})
 
-	display = hub75.New(machine.SPI0, machine.D6, machine.D5, machine.D8, machine.D10, machine.D9, machine.D7)
-	display.Configure(hub75.Config{
+	game.display = hub75.New(machine.SPI0, machine.D6, machine.D5, machine.D8, machine.D10, machine.D9, machine.D7)
+	game.display.Configure(hub75.Config{
 		Width:      64,
 		Height:     32,
 		RowPattern: 16,
 		ColorDepth: 2,
 		FastUpdate: true,
 	})
+	game.display.SetBrightness(100)
+	game.display.ClearDisplay()
 
-	rtc = ds3231.New(machine.I2C0)
-	rtc.Configure()
+	game.rtc = ds3231.New(machine.I2C0)
+	game.rtc.Configure()
 
 	machine.InitADC()
-	player1Pin := machine.ADC{machine.A1}
-	player1Pin.Configure()
+	game.players[0].pin = machine.ADC{machine.A1}
+	game.players[0].pin.Configure()
 
-	player2Pin := machine.ADC{machine.A2}
-	player2Pin.Configure()
+	game.players[1].pin = machine.ADC{machine.A2}
+	game.players[1].pin.Configure()
 
 	// Check if the RTC is working properly
-	valid := rtc.IsTimeValid()
+	valid := game.rtc.IsTimeValid()
 	if !valid {
 		date := time.Date(2019, 12, 13, 20, 16, 22, 0, time.UTC)
-		rtc.SetTime(date)
+		game.rtc.SetTime(date)
 	}
 
-	running := rtc.IsRunning()
+	running := game.rtc.IsRunning()
 	if !running {
-		err := rtc.SetRunning(true)
+		err := game.rtc.SetRunning(true)
 		if err != nil {
 			println("Error configuring RTC")
 		}
 	}
 
-	colors = []color.RGBA{
+	game.colors = []color.RGBA{
 		{255, 0, 0, 255},
 		{255, 255, 0, 255},
 		{0, 255, 0, 255},
@@ -73,241 +101,348 @@ func main() {
 		{0, 0, 0, 255},
 	}
 
-	display.ClearDisplay()
-	display.SetBrightness(100)
-
-	timeStr = make([]byte, 2)
+	game.timeStr = make([]byte, 2)
 	then := time.Now()
-
-	dt, err = rtc.ReadTime()
-	hour := dt.Hour()
-	minute := dt.Minute()
-	updateTime(uint8(hour), uint8(minute))
-
-	ballX := float32(31)
-	ballY := rand.Float32()*16 + 8
-	leftPlayerTargetY := ballY
-	rightPlayerTargetY := ballY
-	leftPlayerY := int16(8)
-	rightPlayerY := int16(18)
-	ballVX := float32(1)
-	ballVY := float32(0.5)
-	if dt.Second() > 29 {
-		ballVY = -0.5
-	}
-
-	playerLoss := int8(0)
-	gameStopped := uint8(0)
+	game.status = IDLE
 
 	for {
-		if time.Since(then).Nanoseconds() > 8000000 {
+		switch game.status {
+		case PLAY:
+			gameStopped := uint8(0)
+			k := uint8(0)
+
+			game.newRound(0)
+			gameStopped = 0
+
+			for game.status == PLAY {
+				if time.Since(then).Nanoseconds() > 8000000 {
+					then = time.Now()
+					game.display.ClearDisplay()
+
+					if gameStopped < 20 {
+						gameStopped++
+					} else {
+						game.ball.x += game.ball.vx
+						game.ball.y += game.ball.vy
+
+						for k = 0; k < 2; k++ {
+							if (k == 1 && game.ball.x >= 60) || (k == 0 && game.ball.x <= 2) {
+								if int16(game.ball.y) < game.players[k].y-1 || int16(game.ball.y) > game.players[k].y+7 {
+									game.newRound((k + 1) % 2)
+									gameStopped = 0
+								} else {
+									game.ball.vx = -game.ball.vx
+									game.ball.vy += (game.ball.y - float32(game.players[k].y) - 3) * 0.2
+									if game.ball.vy > 2.5 {
+										game.ball.vy = 2.5
+									}
+									if game.ball.vy < -2.5 {
+										game.ball.vy = -2.5
+									}
+								}
+							}
+						}
+
+						if game.ball.y >= 30 || game.ball.y <= 0 {
+							game.ball.vy = -game.ball.vy
+						}
+
+						if game.ball.y < 0 {
+							game.ball.y = 0
+						}
+						if game.ball.y > 30 {
+							game.ball.y = 30
+						}
+					}
+
+					for k = 0; k < 2; k++ {
+						game.players[k].y = int16((65535-game.players[k].pin.Get())/2048) - 4
+						if game.players[k].y < 0 {
+							game.players[k].y = 0
+						}
+						if game.players[k].y > 24 {
+							game.players[k].y = 24
+						}
+					}
+
+					// show stuff on the display
+					game.drawNet()
+					game.drawPlayers()
+					game.drawBall()
+					game.updateScore(game.players[0].score, game.players[1].score, false)
+				}
+				game.display.Display()
+			}
+			break
+		case WINNER:
+			game.display.ClearDisplay()
+			game.status = IDLE
 			then = time.Now()
-			display.ClearDisplay()
-
-			if gameStopped < 20 {
-				gameStopped++
+			if game.players[0].score>=50 {
+				tinyfont.WriteLine(&game.display, &tinyfont.TomThumb, 10, 12, []byte("PLAYER 1"), game.colors[2])
 			} else {
-
-				ballX += ballVX
-				ballY += ballVY
-
-				if (ballX >= 60 && playerLoss != 1) || (ballX <= 2 && playerLoss != -1) {
-					ballVX = -ballVX
-					tmp := rand.Int31n(4) // perform a random, last second flick to inflict effect on the ball
-					if tmp > 0 {
-						tmp = rand.Int31n(2)
-						if tmp == 0 {
-							if ballVY > 0 && ballVY < 2.5 {
-								ballVY += 0.2
-							} else if ballVY < 0 && ballVY > -2.5 {
-								ballVY -= 0.2
-							}
-							if ballX >= 60 {
-								rightPlayerTargetY += 1 + 3*rand.Float32()
-							} else {
-								leftPlayerTargetY += 1 + 3*rand.Float32()
-							}
-						} else {
-							if ballVY > 0.5 {
-								ballVY -= 0.2
-							} else if ballVY < -0.5 {
-								ballVY += 0.2
-							}
-							if ballX >= 60 {
-								rightPlayerTargetY -= 1 + 3*rand.Float32()
-							} else {
-								leftPlayerTargetY -= 1 + 3*rand.Float32()
-							}
-						}
-						if leftPlayerTargetY < 0 {
-							leftPlayerTargetY = 0
-						}
-						if leftPlayerTargetY > 24 {
-							leftPlayerTargetY = 24
-						}
-						if rightPlayerTargetY < 0 {
-							rightPlayerTargetY = 0
-						}
-						if rightPlayerTargetY > 24 {
-							rightPlayerTargetY = 24
-						}
-					}
-				} else if (ballX > 62 && playerLoss == 1) || (ballX < 0 && playerLoss == -1) {
-					// RESET GAME
-					ballX = float32(31)
-					ballY = rand.Float32()*16 + 8
-					ballVX = float32(1)
-					ballVY = float32(0.5)
-					if rand.Int31n(2) == 0 {
-						ballVY = -0.5
-					}
-					hour = dt.Hour()
-					minute = dt.Minute()
-					updateTime(uint8(hour), uint8(minute))
-					playerLoss = 0
-					gameStopped = 0
+				tinyfont.WriteLine(&game.display, &tinyfont.TomThumb, 10, 12, []byte("PLAYER 2"), game.colors[2])
+			}
+			tinyfont.WriteLine(&game.display, &tinyfont.TomThumb, 30, 24, []byte("WINS"), game.colors[3])
+			game.players[0].score = 0
+			game.players[1].score = 0
+			game.players[0].lastY = int16(game.players[0].pin.Get() / 16384)
+			game.players[1].lastY = int16(game.players[1].pin.Get() / 16384)
+			for {
+				if time.Since(then).Seconds() > 5 {
+					break
 				}
-				if ballY >= 30 || ballY <= 0 {
-					ballVY = -ballVY
-				}
-
-				// when the ball is on the other side of the court, move the player "randomly" to simulate an AI
-				if ballX == float32(40+rand.Int31n(13)) {
-					leftPlayerTargetY = ballY - 3
-					if leftPlayerTargetY < 0 {
-						leftPlayerTargetY = 0
-					}
-					if leftPlayerTargetY > 24 {
-						leftPlayerTargetY = 24
-					}
-				}
-				if ballX == float32(8+rand.Int31n(13)) {
-					rightPlayerTargetY = ballY - 3
-					if rightPlayerTargetY < 0 {
-						rightPlayerTargetY = 0
-					}
-					if rightPlayerTargetY > 24 {
-						rightPlayerTargetY = 24
-					}
-				}
-
-				if int16(leftPlayerTargetY) > leftPlayerY {
-					leftPlayerY++
-				} else if int16(leftPlayerTargetY) < leftPlayerY {
-					leftPlayerY--
-				}
-
-				if int16(rightPlayerTargetY) > rightPlayerY {
-					rightPlayerY++
-				} else if int16(rightPlayerTargetY) < rightPlayerY {
-					rightPlayerY--
-				}
-
-				// If the ball is in the middle, check if we need to lose and calculate the endpoint to avoid/hit the ball
-				if ballX == 32 {
-
-					dt, err = rtc.ReadTime()
-					if err != nil {
-						println("Error reading date:", err)
-						return
-					}
-					if minute != dt.Minute() && playerLoss == 0 { // needs to change one or the other
-						if dt.Minute() == 0 { // need to change hour
-							playerLoss = 1
-						} else { // need to change the minute
-							playerLoss = -1
-						}
-					}
-
-					if ballVX < 0 { // moving to the left
-						leftPlayerTargetY = calculateEndPoint(ballX, ballY, ballVX, ballVY, playerLoss != -1) - 3
-						if playerLoss == -1 { // we need to lose
-							if leftPlayerTargetY < 16 {
-								leftPlayerTargetY = 19 + 5*rand.Float32()
-							} else {
-								leftPlayerTargetY = 5 * rand.Float32()
-							}
-						}
-						if leftPlayerTargetY < 0 {
-							leftPlayerTargetY = 0
-						}
-						if leftPlayerTargetY > 24 {
-							leftPlayerTargetY = 24
-						}
-					}
-					if ballVX > 0 { // moving to the right
-						rightPlayerTargetY = calculateEndPoint(ballX, ballY, ballVX, ballVY, playerLoss != 1) - 3
-						if playerLoss == 1 { // we need to lose
-							if rightPlayerTargetY < 16 {
-								rightPlayerTargetY = 19 + 5*rand.Float32()
-							} else {
-								rightPlayerTargetY = 5 * rand.Float32()
-							}
-						}
-						if rightPlayerTargetY < 0 {
-							rightPlayerTargetY = 0
-						}
-						if rightPlayerTargetY > 24 {
-							rightPlayerTargetY = 24
-						}
-					}
-				}
-
-				if ballY < 0 {
-					ballY = 0
-				}
-				if ballY > 30 {
-					ballY = 30
-				}
+				game.display.Display()
 			}
 
-			println(player1Pin.Get(), player2Pin.Get())
+			break
+		case IDLE:
+			game.dt, err = game.rtc.ReadTime()
+			hour := game.dt.Hour()
+			minute := game.dt.Minute()
+			game.updateScore(uint8(hour), uint8(minute), true)
 
-			leftPlayerY = int16(player1Pin.Get()/2048) - 4
-			rightPlayerY = int16(player2Pin.Get()/2048) - 4
-			if leftPlayerY < 0 {
-				leftPlayerY = 0
-			}
-			if leftPlayerY > 24 {
-				leftPlayerY = 24
-			}
-			if rightPlayerY < 0 {
-				rightPlayerY = 0
-			}
-			if rightPlayerY > 24 {
-				rightPlayerY = 24
+			game.ball.x = float32(31)
+			game.ball.y = rand.Float32()*16 + 8
+			game.players[0].targetY = game.ball.y
+			game.players[1].targetY = game.ball.y
+			game.players[0].y = int16(8)
+			game.players[1].y = int16(18)
+			game.ball.vx = float32(1)
+			game.ball.vy = float32(0.5)
+			if game.dt.Second() > 29 {
+				game.ball.vy = -0.5
 			}
 
-			// show stuff on the display
-			drawNet()
-			drawPlayer(0, leftPlayerY)
-			drawPlayer(62, rightPlayerY)
-			drawBall(int16(ballX), int16(ballY))
-			updateTime(uint8(hour), uint8(minute))
+			playerLoss := int8(0)
+			gameStopped := uint8(0)
+
+			for game.status == IDLE {
+				if time.Since(then).Nanoseconds() > 8000000 {
+					then = time.Now()
+					game.display.ClearDisplay()
+
+					if gameStopped < 20 {
+						gameStopped++
+					} else {
+
+						game.ball.x += game.ball.vx
+						game.ball.y += game.ball.vy
+
+						if (game.ball.x >= 60 && playerLoss != 1) || (game.ball.x <= 2 && playerLoss != -1) {
+							game.ball.vx = -game.ball.vx
+							tmp := rand.Int31n(4) // perform a random, last second flick to inflict effect on the ball
+							if tmp > 0 {
+								tmp = rand.Int31n(2)
+								if tmp == 0 {
+									if game.ball.vy > 0 && game.ball.vy < 2.5 {
+										game.ball.vy += 0.2
+									} else if game.ball.vy < 0 && game.ball.vy > -2.5 {
+										game.ball.vy -= 0.2
+									}
+									if game.ball.x >= 60 {
+										game.players[1].targetY += 1 + 3*rand.Float32()
+									} else {
+										game.players[0].targetY += 1 + 3*rand.Float32()
+									}
+								} else {
+									if game.ball.vy > 0.5 {
+										game.ball.vy -= 0.2
+									} else if game.ball.vy < -0.5 {
+										game.ball.vy += 0.2
+									}
+									if game.ball.x >= 60 {
+										game.players[1].targetY -= 1 + 3*rand.Float32()
+									} else {
+										game.players[0].targetY -= 1 + 3*rand.Float32()
+									}
+								}
+								if game.players[0].targetY < 0 {
+									game.players[0].targetY = 0
+								}
+								if game.players[0].targetY > 24 {
+									game.players[0].targetY = 24
+								}
+								if game.players[1].targetY < 0 {
+									game.players[1].targetY = 0
+								}
+								if game.players[1].targetY > 24 {
+									game.players[1].targetY = 24
+								}
+							}
+						} else if (game.ball.x > 62 && playerLoss == 1) || (game.ball.x < 0 && playerLoss == -1) {
+							// RESET GAME
+							game.ball.x = float32(31)
+							game.ball.y = rand.Float32()*16 + 8
+							game.ball.vx = float32(1)
+							game.ball.vy = float32(0.5)
+							if rand.Int31n(2) == 0 {
+								game.ball.vy = -0.5
+							}
+							hour = game.dt.Hour()
+							minute = game.dt.Minute()
+							game.updateScore(uint8(hour), uint8(minute), true)
+							playerLoss = 0
+							gameStopped = 0
+						}
+						if game.ball.y >= 30 || game.ball.y <= 0 {
+							game.ball.vy = -game.ball.vy
+						}
+
+						// when the ball is on the other side of the court, move the player "randomly" to simulate an AI
+						if game.ball.x == float32(40+rand.Int31n(13)) {
+							game.players[0].targetY = game.ball.y - 3
+							if game.players[0].targetY < 0 {
+								game.players[0].targetY = 0
+							}
+							if game.players[0].targetY > 24 {
+								game.players[0].targetY = 24
+							}
+						}
+						if game.ball.x == float32(8+rand.Int31n(13)) {
+							game.players[1].targetY = game.ball.y - 3
+							if game.players[1].targetY < 0 {
+								game.players[1].targetY = 0
+							}
+							if game.players[1].targetY > 24 {
+								game.players[1].targetY = 24
+							}
+						}
+
+						if int16(game.players[0].targetY) > game.players[0].y {
+							game.players[0].y++
+						} else if int16(game.players[0].targetY) < game.players[0].y {
+							game.players[0].y--
+						}
+
+						if int16(game.players[1].targetY) > game.players[1].y {
+							game.players[1].y++
+						} else if int16(game.players[1].targetY) < game.players[1].y {
+							game.players[1].y--
+						}
+
+						// If the ball is in the middle, check if we need to lose and calculate the endpoint to avoid/hit the ball
+						if game.ball.x == 32 {
+
+							game.dt, err = game.rtc.ReadTime()
+							if err != nil {
+								println("Error reading date:", err)
+								return
+							}
+							if minute != game.dt.Minute() && playerLoss == 0 { // needs to change one or the other
+								if game.dt.Minute() == 0 { // need to change hour
+									playerLoss = 1
+								} else { // need to change the minute
+									playerLoss = -1
+								}
+							}
+
+							if game.ball.vx < 0 { // moving to the left
+								game.players[0].targetY = calculateEndPoint(game.ball.x, game.ball.y, game.ball.vx, game.ball.vy, playerLoss != -1) - 3
+								if playerLoss == -1 { // we need to lose
+									if game.players[0].targetY < 16 {
+										game.players[0].targetY = 19 + 5*rand.Float32()
+									} else {
+										game.players[0].targetY = 5 * rand.Float32()
+									}
+								}
+								if game.players[0].targetY < 0 {
+									game.players[0].targetY = 0
+								}
+								if game.players[0].targetY > 24 {
+									game.players[0].targetY = 24
+								}
+							}
+							if game.ball.vx > 0 { // moving to the right
+								game.players[1].targetY = calculateEndPoint(game.ball.x, game.ball.y, game.ball.vx, game.ball.vy, playerLoss != 1) - 3
+								if playerLoss == 1 { // we need to lose
+									if game.players[1].targetY < 16 {
+										game.players[1].targetY = 19 + 5*rand.Float32()
+									} else {
+										game.players[1].targetY = 5 * rand.Float32()
+									}
+								}
+								if game.players[1].targetY < 0 {
+									game.players[1].targetY = 0
+								}
+								if game.players[1].targetY > 24 {
+									game.players[1].targetY = 24
+								}
+							}
+						}
+
+						if game.ball.y < 0 {
+							game.ball.y = 0
+						}
+						if game.ball.y > 30 {
+							game.ball.y = 30
+						}
+					}
+
+					if game.players[0].lastY != int16(game.players[0].pin.Get()/16384) ||
+						game.players[1].lastY != int16(game.players[1].pin.Get()/16384) {
+						game.status = PLAY
+					}
+					game.players[0].lastY = int16(game.players[0].pin.Get() / 16384)
+					game.players[1].lastY = int16(game.players[1].pin.Get() / 16384)
+
+					// show stuff on the display
+					game.drawNet()
+					game.drawPlayers()
+					game.drawBall()
+					game.updateScore(uint8(hour), uint8(minute), true)
+				}
+				game.display.Display()
+			}
+			break
 		}
-		display.Display()
 	}
 }
 
-func drawNet() {
+func (g *Game) newRound(winner uint8) {
+	g.ball.x = float32(31)
+	g.ball.y = rand.Float32()*16 + 8
+	g.ball.vx = float32(1)
+	if winner == 0 {
+		g.ball.vy = 0.5
+	} else {
+		g.ball.vy = -0.5
+	}
+	g.players[winner].score++
+	if g.players[winner].score > 50 {
+		g.status = WINNER
+	}
+}
+
+func (g *Game) drawNet() {
 	for i := int16(1); i < 32; i += 2 {
-		display.SetPixel(31, i, colors[6])
+		g.display.SetPixel(31, i, g.colors[6])
 	}
 }
 
-func drawPlayer(x int16, y int16) {
+func (g *Game) drawPlayers() {
+	for k := int16(0); k < 2; k++ {
+		for i := int16(0); i < 2; i++ {
+			for j := int16(0); j < 8; j++ {
+				g.display.SetPixel((k*62)+i, g.players[k].y+j, g.colors[3])
+			}
+		}
+	}
+}
+
+func (g *Game) drawPlayer(x int16, y int16) {
 	for i := int16(0); i < 2; i++ {
 		for j := int16(0); j < 8; j++ {
-			display.SetPixel(x+i, y+j, colors[3])
+			g.display.SetPixel(x+i, y+j, g.colors[3])
 		}
 	}
 }
 
-func drawBall(x int16, y int16) {
-	display.SetPixel(x, y, colors[1])
-	display.SetPixel(x+1, y, colors[1])
-	display.SetPixel(x, y+1, colors[1])
-	display.SetPixel(x+1, y+1, colors[1])
+func (g *Game) drawBall() {
+	g.display.SetPixel(int16(g.ball.x), int16(g.ball.y), g.colors[1])
+	g.display.SetPixel(int16(g.ball.x)+1, int16(g.ball.y), g.colors[1])
+	g.display.SetPixel(int16(g.ball.x), int16(g.ball.y)+1, g.colors[1])
+	g.display.SetPixel(int16(g.ball.x)+1, int16(g.ball.y)+1, g.colors[1])
 }
 
 func calculateEndPoint(x float32, y float32, vx float32, vy float32, hit bool) (ty float32) {
@@ -329,20 +464,24 @@ func calculateEndPoint(x float32, y float32, vx float32, vy float32, hit bool) (
 	}
 }
 
-func updateTime(hour uint8, minute uint8) {
-	timeStr[1] = 48 + (hour % 10)
-	if hour > 9 {
-		timeStr[0] = 48 + (hour / 10)
+func (g *Game) updateScore(scoreLeft uint8, scoreRight uint8, showTime bool) {
+	g.timeStr[1] = 48 + (scoreLeft % 10)
+	if scoreLeft > 9 {
+		g.timeStr[0] = 48 + (scoreLeft / 10)
 	} else {
-		timeStr[0] = 32
+		g.timeStr[0] = 32
 	}
-	tinyfont.WriteLine(&display, &tinyfont.TomThumb, 23, 5, timeStr, colors[6])
+	tinyfont.WriteLine(&g.display, &tinyfont.TomThumb, 23, 5, g.timeStr, g.colors[6])
 
-	timeStr[1] = 48 + (minute % 10)
-	if minute > 9 {
-		timeStr[0] = 48 + (minute / 10)
+	g.timeStr[1] = 48 + (scoreRight % 10)
+	if scoreRight > 9 {
+		g.timeStr[0] = 48 + (scoreRight / 10)
 	} else {
-		timeStr[0] = 48
+		if showTime {
+			g.timeStr[0] = 48
+		} else {
+			g.timeStr[0] = 32
+		}
 	}
-	tinyfont.WriteLine(&display, &tinyfont.TomThumb, 33, 5, timeStr, colors[6])
+	tinyfont.WriteLine(&g.display, &tinyfont.TomThumb, 33, 5, g.timeStr, g.colors[6])
 }
